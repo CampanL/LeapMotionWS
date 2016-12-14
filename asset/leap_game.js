becce23cefaa43597cdb2f69be4f3ffe66b5494d
SemoonTheLord <bastien.lhuaire@outlook.fr>
2016-12-14 10:55:41 +0100

    factorisation du shield

--- asset/leap_game.js
+++ asset/leap_game.js
@@ -5,49 +5,31 @@ Leap.loop(function(frame){
   frame.hands.forEach(function(hand) {
   	normal = hand.palmNormal;
   	//console.log(normal);
-
+	let shield = {};
+	if (hand.type=="left") {shield=shield_l}
+  	else if (hand.type=="right") {shield=shield_r};
   	if (normal[2]<=-0.7) {
-  		console.log(hand)
-  		if (hand.type=="left") {
-  			scene.remove(shield_l);
-			shield_l.position.x=hand.palmPosition[0];
-			shield_l.position.y=hand.palmPosition[1];
-			shield_l.position.z=hand.palmPosition[2];
-			scene.add(shield_l);
-		}
-		else if (hand.type=="right") {
-  			scene.remove(shield_r);
-			shield_r.position.x=hand.palmPosition[0];
-			shield_r.position.y=hand.palmPosition[1];
-			shield_r.position.z=hand.palmPosition[2];
-			scene.add(shield_r);
-		}
+  		scene.remove(shield_l);
+		shield.position.x=hand.palmPosition[0];
+		shield.position.y=hand.palmPosition[1];
+		shield.position.z=hand.palmPosition[2];
+		scene.add(shield);
   	}else{
   		//cache le bouclier
-  		if (hand.type=="left") {
-			shield_l.position.x=0;
-			shield_l.position.y=0;
-			shield_l.position.z=-1000;
-		}
-		else if (hand.type=="right") {
-			shield_r.position.x=0;
-			shield_r.position.y=0;
-			shield_r.position.z=-1000;
-		}
+		shield.position.x=0;
+		shield.position.y=0;
+		shield.position.z=-1000;
   	}
   });
 }).on('handLost',
     function(hand){
     	//cache le bouclier
-    	if (hand.type=="left") {
-	  		shield_l.position.x=0;
-			shield_l.position.y=0;
-			shield_l.position.z=-1000;
-		}else if(hand.type=="right"){
-			shield_r.position.x=0;
-			shield_r.position.y=0;
-			shield_r.position.z=-1000;
-		}
+    	let shield = {};
+  		if (hand.type=="left") {shield=shield_l}
+  		else if (hand.type=="right") {shield=shield_r};
+	  	shield.position.x=0;
+		shield.position.y=0;
+		shield.position.z=-1000;
 	})
 .use('transform', {
   // move 20 cm back.
