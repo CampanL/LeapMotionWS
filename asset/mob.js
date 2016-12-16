d8c2d3d0fe68f8d9b00270479fd199458d5c2fa3
Loan <campan.loan78@gmail.com>
2016-12-16 11:06:40 +0100

    ajout du sprite de tuto

--- asset/mob.js
+++ asset/mob.js
@@ -6,20 +6,20 @@ const nbMob = 1;
 for (var i = 0; i < nbMob; i++) {
   mobs.push( mob({
     x : random(-150, 150),
-    y : random(-100, 100)
+    y : random(-50, 50)
   }));
 }
 
 function mob(pos)
 {
 	var geometry = new THREE.SphereGeometry( 25, 32, 32 );
-	var material = new THREE.MeshLambertMaterial( {color: 0xff0000} );
+	var material = new THREE.MeshLambertMaterial( {color: 0x000000} );
 	var sphere = new THREE.Mesh( geometry, material );
 	scene.add( sphere );
 
 	sphere.position.x=pos.x;
 	sphere.position.y=pos.y;
-	sphere.position.z=-500;
+	sphere.position.z=-750;
 
 	return sphere;
 }
--- asset/render.js
+++ asset/render.js
@@ -1,73 +1,103 @@
 var compteur = 0;
-
+var inter_shoot=100;
+var speed = 3;
 var randx=0;
 var randy=0;
 
 function render() {
   requestAnimationFrame( render );
 
-  // Animation des cubes
-  for (var i = 0, cube; i < cubes.length; i++) {
-    cube = cubes[i];
-
-    cube.position.z += 3;
-    cube.position.x += (camera.position.x - cube.position.x)/200;
-    cube.position.y += (camera.position.y - cube.position.y)/100;
-
-    if (cube.position.z > 20) {
-      scene.remove(cube);
-    }
-
-
-  }
-  //bouge le shot r et vérifie la colision avec le fond
-  if (shot_r.shooted) {
-    shot_sphere_r.position.z -=shot_r.speed;
-    if (shot_sphere_r.position.z < -1300) {
-      shot_r.shooted = false;
-      shot_r.ready = false;
-      shot_r.size=0.5;
-    }
-  }
-
-  //bouge le shot l et vérifie la colision avec le fond
-  if (shot_l.shooted) {
-    shot_sphere_l.position.z -=shot_l.speed;
-    if (shot_sphere_l.position.z < -1300) {
-      shot_l.shooted = false;
-      shot_l.ready = false;
-      shot_l.size=0.5;
-    }
-  }  
-
-  //gestion des mobs
-  for (var j=0, mob; j < mobs.length; j++)
+  //affichage du tutoriel
+  if (tuto==true) 
   {
-    mob = mobs[j];
-
-    if (compteur == 0) 
+    mobs[0].position.x=200;
+    mobs[0].position.y=250;
+    compteur++;
+    if (compteur>150) 
     {
-      randx = random(-2,2);
-      randy = random(-2,2);
-      mob_shoot(mob);
+      mob_shoot(mobs[0])
+      compteur=0;
     }
 
-    mob.position.x += randx;
-    mob.position.y += randy;
-    
-    if (mob.position.x < -350 || mob.position.x > 350) 
+    for (var i = 0, cube; i < cubes.length; i++) 
     {
-      randx = -randx;
+      cube = cubes[i];
+
+      cube.position.z += 3;
+      cube.position.x += (camera.position.x - cube.position.x)/200;
+      cube.position.y += (camera.position.y - cube.position.y)/100;
+
+      if (cube.position.z > 20) {
+        scene.remove(cube);
+      }
     }
-    if (mob.position.y < -300 || mob.position.y > 300) 
-    {
-      randy = -randy;
-    }
-  }
-  compteur++;
-  if (compteur>100) 
+
+  }else
   {
-    compteur = 0;
+
+    // Animation des cubes
+    for (var i = 0, cube; i < cubes.length; i++) 
+    {
+      cube = cubes[i];
+
+      cube.position.z += 3;
+      cube.position.x += (camera.position.x - cube.position.x)/200;
+      cube.position.y += (camera.position.y - cube.position.y)/100;
+
+      if (cube.position.z > 20) {
+        scene.remove(cube);
+      }
+    }
+    //bouge le shot r et vérifie la colision avec le fond
+    if (shot_r.shooted) {
+      shot_sphere_r.position.z -=shot_r.speed;
+      if (shot_sphere_r.position.z < -1300) {
+        shot_r.shooted = false;
+        shot_r.ready = false;
+        shot_r.size=0.5;
+      }
+    }
+
+    //bouge le shot l et vérifie la colision avec le fond
+    if (shot_l.shooted) {
+      shot_sphere_l.position.z -=shot_l.speed;
+      if (shot_sphere_l.position.z < -1300) {
+        shot_l.shooted = false;
+        shot_l.ready = false;
+        shot_l.size=0.5;
+      }
+    }  
+
+    //gestion des mobs
+    for (var j=0, mob; j < mobs.length; j++)
+    {
+      mob = mobs[j];
+
+      if (compteur == 0) 
+      {
+        randx = random(-2,2);
+        randy = random(-2,2);
+        mob_shoot(mob);
+      }
+
+      mob.position.x += randx;
+      mob.position.y += randy;
+      
+      if (mob.position.x < -350 || mob.position.x > 350) 
+      {
+        randx = -randx;
+      }
+      if (mob.position.y < -100 || mob.position.y > 100) 
+      {
+        randy = -randy;
+      }
+        console.log(mob.position.y);
+    }
+    compteur++;
+    if (compteur>100) 
+    {
+      compteur = 0;
+    }
   }
   // Rendering...
   renderer.render( scene, camera );
