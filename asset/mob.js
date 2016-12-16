98489cc350815cad5bdb7390a92b7d378c8c64ce
SemoonTheLord <bastien.lhuaire@outlook.fr>
2016-12-16 13:59:43 +0100

    ajout de la page loose

--- asset/mob.js
+++ asset/mob.js
@@ -11,7 +11,7 @@ for (var i = 0; i < nbMob; i++) {
 function mob(pos)
 {
 	var geometry = new THREE.SphereGeometry( 25, 32, 32 );
-	var material = new THREE.MeshLambertMaterial( {color: 0xff0000} );
+	var material = new THREE.MeshLambertMaterial( {color: 0x000000} );
 	var sphere = new THREE.Mesh( geometry, material );
 	scene.add( sphere );
 
